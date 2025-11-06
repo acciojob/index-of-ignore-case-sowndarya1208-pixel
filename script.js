e702d7042a169bbefc38ddab42caa8d9a1fb2b70
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-06 18:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-
     // Convert both strings to lowercase for case-insensitive comparison
     const lowerS1 = s1.toLowerCase();
     const lowerS2 = s2.toLowerCase();
     
     // Use indexOf to find the position
     return lowerS1.indexOf(lowerS2);
+	if (!lowerS1. includes(lowerS2)) {
+		return -1;
+		
+	}
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
-console. log(indexOfIgnoreCase("Hello World", "world"));
-}
-
-// Please do not change the code below
-
+console. log(indexOfIgnoreCase("Hello World","World"));
